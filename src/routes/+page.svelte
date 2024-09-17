08e608df2af338af78eab814770060d53415c672
nivetha-balu <nivethabalucva@gmail.com>
2024-09-17 20:49:29 +0530

    misc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <main class="bg-gradient-to-r from-gray-900 to-pink-800 flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-6">
-    <section class="max-w-4xl w-full text-center">
-      <div class="mb-12">
-        <h1 class="text-4xl md:text-6xl font-bold mb-4">Nivetha Balu</h1>
-        <p class="text-lg md:text-xl font-light"> Data Scientist | AI Engineer</p>
-      </div>
-      <div class="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-6">
-        <a href="#about" class="px-6 py-3 bg-blue-600 hover:bg-blue-700 rounded-lg text-lg transition">About Me</a>
-        <a href="#projects" class="px-6 py-3 bg-green-600 hover:bg-green-700 rounded-lg text-lg transition">Projects</a>
-        <a href="https://www.linkedin.com/in/nivetha98/" class="px-6 py-3 bg-purple-600 hover:bg-purple-700 rounded-lg text-lg transition">LinkedIn</a>
-      </div>
-    </section>
-  </main>
+  <section class="max-w-4xl w-full text-center">
+    <div class="mb-12">
+      <h1 class="text-4xl md:text-6xl font-bold mb-4">Nivetha Balu</h1>
+      <p class="text-lg md:text-xl font-light"> Data Scientist | AI Engineer</p>
+    </div>
+    <div class="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-6">
+      <a href="#about" class="px-6 py-3 bg-blue-600 hover:bg-blue-700 rounded-lg text-lg transition">About Me</a>
+      <a href="#projects" class="px-6 py-3 bg-green-600 hover:bg-green-700 rounded-lg text-lg transition">Projects</a>
+      <a href="https://www.linkedin.com/in/nivetha98/" class="px-6 py-3 bg-purple-600 hover:bg-purple-700 rounded-lg text-lg transition">LinkedIn</a>
+    </div>
+  </section>
+</main>
