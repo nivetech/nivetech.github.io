33895ce1c907441ae2209b0bc197291b447a3ab7
nivetha-balu <nivethabalucva@gmail.com>
2025-04-27 18:49:29 +0530

    Converted website from svelte to tailwindCSS and HTML - basic version

--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nivetha's Playground</title>
-    <meta charset="UTF-8">
-    <script src="https://cdn.tailwindcss.com"></script>
-</svelte:head>
-<slot/>
