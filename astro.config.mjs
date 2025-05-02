b13160ea1f99937152412b7867e917f09ec787f6
aditya-xq <adityavivek1998@gmail.com>
2025-05-02 23:34:30 +0530

    Updated deployment configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nivetech.github.io',
-  base: '/',
   outDir: './dist',
   vite: {
     plugins: [tailwindcss()]
